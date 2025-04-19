0ac9a4d2967e24d5fb2ceb1648a5a6f0cfd145e3
João Victor Farchi <jfarchi@sga.pucminas.br>
2025-04-19 15:57:35 -0300

    Alterando descricao de funcao principal na tela inicial

--- components/hero.tsx
+++ components/hero.tsx
@@ -10,7 +10,7 @@ const Hero = ({ pdfLoaded }: HeroProps) => {
     <section id="home" className={styles.hero}>
       <div className={styles.heroContent}>
         <h1 className={styles.name}>João Victor Farchi</h1>
-        <h2 className={styles.title}>Software Developer I</h2>
+        <h2 className={styles.title}>Software Developer</h2>
         <p className={styles.description}>Criando soluções inovadoras com paixão e precisão</p>
         <div className={styles.buttonContainer}>
           <a href="mailto:jv.farchi@hotmail.com" className="btn-3d">
